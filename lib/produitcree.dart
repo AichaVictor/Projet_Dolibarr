5a988cd0a05b5a052e06f175d792bebcadcaf963
MariemeSougoufara <Mariemesgpro@gmail.com>
2023-02-19 00:04:01 +0000

    last

---
+++ lib/produitcree.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class Details extends StatelessWidget {
+  Details({Key? key, required this.productName}) : super(key: key);
+
+  String productName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Liste des produits"),
+        backgroundColor: Color(0xff453658),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.account_balance_wallet_outlined),
+              title: Text(productName),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/stockproduit.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'createProduit.dart';
+import 'produitcree.dart';
+import 'package:http/http.dart' as http;
+
+class LDetails extends StatelessWidget {
+  LDetails({Key? key, required this.productName}) : super(key: key);
+
+  String productName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Liste des produits"),
+        backgroundColor: Color(0xff453658),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            ListTile(
+              leading: Icon(Icons.account_balance_wallet_outlined),
+              title: Text(productName),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
