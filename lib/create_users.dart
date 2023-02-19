6c044cb076d81ac07c8fb9b80b6071eb553ab14a
MariemeSougoufara <Mariemesgpro@gmail.com>
2023-02-19 00:07:23 +0000

    last1

---
+++ lib/create_users.dart
@@ -0,0 +1,219 @@
+import 'package:dolibarr/createProduit.dart';
+import 'package:flutter/material.dart';
+import 'profil_utilisateur.dart';
+import 'package:dio/dio.dart';
+
+Dio dio = Dio();
+const apiUrl = 'https://dolibarr.with6.dolicloud.com/api/index.php/explorer/#!/login/listLogin';
+String apiToken = 'API';
+
+class CreateUsers extends StatefulWidget {
+  const CreateUsers({Key? key}) : super(key: key);
+
+  @override
+  State<CreateUsers> createState() => _CreateUsersState();
+}
+
+class _CreateUsersState extends State<CreateUsers> {
+  bool _isVisible = false;
+  bool _isPassword12 = false;
+  int selected = 0;
+  final formKey = GlobalKey<FormState>();
+  final nomController = TextEditingController();
+  final prenomController = TextEditingController();
+  final identifiantController = TextEditingController();
+  final adresseController = TextEditingController();
+  final motDePasseController = TextEditingController();
+
+  onPasswordChanged(String password){
+    setState(() {
+      _isPassword12 = false;
+      if(password.length >= 12)
+        _isPassword12 = true;
+    });
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    nomController.dispose();
+    prenomController.dispose();
+    identifiantController.dispose();
+    adresseController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text("Nouvel utilisateur"),
+        backgroundColor : Colors.purple,
+      ),
+      body: Form(
+          key: formKey,
+          child: ListView(children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 45.0, left: 18.0, right: 18.0, bottom: 8.0),
+              child: TextFormField(
+                  controller: nomController,
+                  decoration:  InputDecoration(
+                      labelText: 'Nom',
+                      border: OutlineInputBorder(
+                        borderRadius:
+                        BorderRadius.all(Radius.circular(20.0)),)),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Veuiller saisir votre nom';
+                    }
+                    return null;
+                  }),
+            ),
+            Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 18.0, right: 18.0, bottom: 8.0),
+                child: TextFormField(
+                  controller: prenomController,
+                  decoration:  InputDecoration(
+                      labelText: 'Prenom',
+                      border: OutlineInputBorder(
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(20.0)))),
+                )),
+            Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 18.0, right: 18.0, bottom: 8.0),
+                child: TextFormField(
+                    controller: identifiantController,
+                    decoration:  InputDecoration(
+                        labelText: 'Identifiant',
+                        border: OutlineInputBorder(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(20.0)))),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Veuiller saisir votre identifiant';
+                      }
+                      return null;
+                    }
+                )),
+            Padding(
+                padding: const EdgeInsets.only(
+                    top: 16.0, left: 18.0, right: 18.0, bottom: 8.0),
+                child: TextFormField(
+                  controller: adresseController,
+                  decoration:  InputDecoration(
+                      labelText: 'Adresse',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)))),
+                  keyboardType: TextInputType.multiline,
+                  minLines: 2,
+                  maxLines: 3,
+                )),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 16.0, left: 18.0, right: 18.0, bottom: 8.0) ,
+              child: DropdownButtonFormField(
+                  items: [
+                    DropdownMenuItem(value: 1 ,child: Text("Oui")),
+                    DropdownMenuItem(value: 0 ,child: Text("Non"))
+                  ],
+                  decoration: InputDecoration(
+                      labelText: 'Administrateur',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0))
+                      )),
+                  value: selected,
+                  onChanged: (value){
+                    setState(() {
+                      selected = value!;
+                    });
+                  }
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 16.0, left: 18.0, right: 18.0, bottom: 8.0),
+              child: TextFormField(
+                  onChanged: (password) => onPasswordChanged(password),
+                  obscureText: !_isVisible,
+                  controller: motDePasseController,
+                  decoration: InputDecoration(
+                      labelText: 'Mot de passe',
+                      suffixIcon: IconButton(onPressed: (){
+                        setState(() {
+                          _isVisible = !_isVisible;
+                        });
+                      },
+                        icon: _isVisible ? Icon(Icons.visibility, color: Colors.black,) : Icon(Icons.visibility_off, color: Colors.grey,),
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)))),
+                  validator: (value) {
+                    if (value == null || value.isEmpty || _isPassword12 == false) {
+                      return 'Veuiller saisir votre mot de passe, il doit avoir 12'
+                          ' caracteres';
+                    }
+                    return null;
+                  }
+              ),
+            ),
+
+            SizedBox(height: 15,),
+            Row(
+              children: [
+                AnimatedContainer(
+                  duration: Duration(milliseconds: 500),
+                  width: 20,
+                  height: 20,
+                  decoration: BoxDecoration(
+                    color: _isPassword12 ? Colors.green : Colors.transparent,
+                      border: _isPassword12 ? Border.all(color: Colors.transparent ) :
+                      Border.all(color: Colors.grey.shade400),
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Center(child: Icon(Icons.check, color: Colors.white, size: 15,),),
+                ),
+                SizedBox(width: 10,),
+                Text("Contient au moins 12 caracteres")
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  if(formKey.currentState!.validate()){
+
+                    try {
+                      Response response = await dio.post(apiUrl,
+                          data: {
+                        "login": identifiantController,
+                            "password": motDePasseController,
+                            "entity": 1
+                          });
+                      String apiToken = response.data['token'];
+                      print("hello");
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>UserProfilePage(
+                        nom: nomController.text,
+                        prenom: prenomController.text,
+                        identifiant: identifiantController.text,
+                        adresse: adresseController.text,
+                      )));
+                    } catch (e) {
+                      print(e.toString());
+                    }
+
+                }},
+                child: Text('Creer'),
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Annuler'),
+              ),
+            )
+          ])), //form
+    );
+  }
+}
--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,138 +1,80 @@
+import 'package:dolibarr/login2_page.dart';
 import 'package:dolibarr/main.dart';
 import 'package:flutter/material.dart';
- 
 class login_page extends StatelessWidget  {
-   @override
-  Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.grey[300],
+      backgroundColor:Colors.white, 
       body: SafeArea(
-        child: Center (
-        child: Column (
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [ 
-            const Icon(Icons.phone_android_rounded,
-            size: 100,
-            ),
-            const SizedBox(height: 75),
-
-
-            // Hello ! 
-        const Text(
-          'Hello !',
-          style: TextStyle(
-            fontWeight: FontWeight.bold, 
-          fontSize: 36,
-          ),
-          ),
-      const SizedBox(height: 10),
-       const  Text(
-           'Wecome!',
-          style: TextStyle( 
-          fontSize: 20,
-          ),
-          ),
-        const SizedBox(height: 50),
-
-        // Identifiant textfield
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child : Container(
-           decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border:Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: const Padding (
-            padding:  EdgeInsets.only(left: 20.0),
-            child: TextField(
-           decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Identifiant',
-        ),
-        ),
-        ),
-        ),
-        ),
-        const SizedBox(height: 10),
-
-
-        // Mot de Passe textfield 
-          Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child : Container(
-           decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border:Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: const Padding (
-            padding: EdgeInsets.only(left: 20.0),
-            child: TextField(
-              obscureText: true,
-           decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: 'Mot de Passe',
-        ),
-        ),
-        ),
-        ),
-        ),
-       const  SizedBox(height: 10),
-
-
-        // Se connecter
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Container(
-            padding:const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(12),
-              ),
-            child: Center(
-              child: Text(
-                'Se connecter',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget> [
+              Column(
+                children: <Widget> [
+                  Text("Welcome",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
-                ),
-        ),
-        ),
-        ),
-        SizedBox(height: 10),
-
-        // Mot de Passe oublié ? -Besoin d'assistance ou aide ?
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-           Text('Mot de Passe oublié ? ',
-           style: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-           ),
-           ),
-           Text(
-            '-Besoin assistance',
-           style: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-           ),
-           ),
-           Text('ou aide',
-           style: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
-           ),
-           ),
-          ],
-        ),  
-        ],
-      ),
-    ),
-    ),
-    );
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image:AssetImage("assets/login.PNG") 
+                        )
+                    ),
+                    ),
+                    Column(
+                      children: <Widget> [
+                        MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context) => login_page()));
+                        },
+                        
+                        
+                        ),
+                        
+                        SizedBox(height: 20),
+                        MaterialButton(
+                          minWidth: double.infinity,
+                          height: 60,
+                          onPressed: () {
+                            Navigator.push(context,
+                             MaterialPageRoute(builder: (context) => login2()));
+                          },
+                          color: Color(0xff0095FF),
+                          //shape
+                        shape: RoundedRectangleBorder(
+                         borderRadius: BorderRadius.circular(50),
+                               
+                        ),
+                        child: Text("Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                        ),),
+                )
+                      ]          
+                    ),
+                ],
+      
+              ),
+            ],
+      
+      ))));
   }
 }
+      
