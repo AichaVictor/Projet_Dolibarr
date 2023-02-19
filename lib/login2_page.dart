8eb955ab8cd01d79dc56175fa2839786d8066499
Maremesarr <maremesarr@esp.sn>
2023-02-19 14:07:24 +0000

    login page 2 (suite)

--- lib/login2_page.dart
+++ lib/login2_page.dart
@@ -1,139 +1,177 @@
-import 'package:dolibarr/main.dart';
+import 'dart:convert';
+import 'package:dolibarr/utilisateurs.dart';
 import 'package:flutter/material.dart';
-class login2 extends StatefulWidget {
+import 'package:http/http.dart' as http;
+
+
+class Login2 extends StatefulWidget {
   @override
-  _login2 createState() => _login2();
+  _Login2 createState() => _Login2();
 }
-class _login2 extends State<login2>  {
+
+class _Login2 extends State<Login2> {
+  final loginController= TextEditingController();
+  final passController= TextEditingController();
+  
+  final _labelStyle = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: Colors.black87,
+  );
+
+  final _borderStyle = OutlineInputBorder(
+    borderSide: BorderSide(
+      color: Colors.grey[400]!,
+    ),
+  );
+
+  Future<dynamic> loginUser(String login, String password) async {
+  final response = await http.post(
+    Uri.parse('https://dolibarr.with6.dolicloud.com/api/index.php/explorer/login'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, dynamic>{
+      'login': login,
+      'password': password,
+      
+    }),
+  );
+
+  if (response.statusCode == 200) {
+    
+    final data=jsonDecode(response.body);
+    final token = data['success']['token'] as String;
+      final user = User.fromJson(data);
+      setState(() { 
+    });
+    print('connextion reussie');
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+
+
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:Colors.white, 
-      appBar: AppBar(elevation: 0,
-      brightness:Brightness.light,
       backgroundColor: Colors.white,
-      leading: IconButton(
-        onPressed: (){
-          Navigator.pop(context);
-        },
-        icon: Icon(Icons.arrow_back_ios,
-        size: 20,
-        color: Colors.black,
+      appBar: AppBar(
+        elevation: 0,
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: Colors.black,
+          ),
         ),
-      ),
       ),
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: double.infinity,
-        child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-       children: <Widget> [
-        Expanded(child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Text("Login to your account",
-                style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-              SizedBox(height: 20,),
-             
-              ],
-            )
-        ],
-        ),),
-        Padding(
-          padding:
-          EdgeInsets.symmetric(horizontal: 40,),
-          child: Column(
-            children:[
-            inputFile(label: "Identifiant"),
-            inputFile(label: "Passeword", obscureText: true)
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        "Connectez-vous ",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                
+
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller:loginController,
+
+                    obscureText: false,
+                    decoration: InputDecoration(
+                    labelText: "Identifiant", prefixIcon: Align(widthFactor: 1.0,heightFactor: 1.0, child: Icon(Icons.person),),
+                    contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(
+                    color: Colors.grey[400]! 
+                   ),
+                   ),
+                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey[400]!)
+                   )
+                   ),
+                   ),
+                 SizedBox(height: 10,),
+                  
+                 TextFormField(
+                    controller:passController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                    labelText: "Mot de passe",prefixIcon: Align(widthFactor: 1.0,heightFactor: 1.0, child: Icon(Icons.lock),),
+                    contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(
+                    color: Colors.grey[400]! 
+                    ),
+                    ),
+                   border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey[400]!)
+                    )
+                    ),
+                    ),
+                  SizedBox(height: 10,),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: MaterialButton(
+                minWidth: double.infinity,
+                height: 60,
+                onPressed: () {loginUser(loginController.text, passController.text);
+                },
+                color: Color(0xff0095ff),
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Text(
+                  "Se connecter",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 100),
+              height: 200,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/login1.PNG"),
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
           ],
-          ),
-          ),
-        Padding(
-          padding: 
-        EdgeInsets.symmetric(horizontal: 40,),
-          child: Container(
-            padding: EdgeInsets.only(top: 30, left: 3),
-             decoration: 
-          BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          border: Border(
-           bottom: BorderSide(color: Colors.black),
-           top: BorderSide(color: Colors.black),
-           left: BorderSide(color: Colors.black),
-           right: BorderSide(color: Colors.black),
-           
-          )
-          ),
-          child: MaterialButton(
-            minWidth: double.infinity,
-            height: 60,
-            onPressed: (){},
-            color: Color(0xff0095ff),
-            elevation: 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: Text(
-              "Login",
-            style: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 18,
-              color: Colors.white,
-              
-            ),
-            ),
-          ),
-          ),
         ),
-       
-      Container(
-        padding: EdgeInsets.only(top: 100),
-        height: 200,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/login1.PNG"),
-            fit: BoxFit.fitHeight
-          ),
-        ),
-      )
-
-        ,
-       
-       ],
       ),
-    ));
-  }
-}
-
-//we will bi creating a widget for text field
-Widget inputFile({label, obscureText = false})
-{
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Text(
-        label,
-        style: TextStyle(fontSize: 15,
-         fontWeight: FontWeight.w400,
-         color: Colors.black87
-        ),),
-        SizedBox(height: 5,),
-        TextField(obscureText: obscureText,
-        decoration: InputDecoration(contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-          color: Colors.grey[400]! 
-          ),
-        ),
-          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey[400]!)
-          )
-          ),
-           ),
-           SizedBox(height: 10,)
-        
-    ]
-  );
-  
-}
+    );
+  }}
