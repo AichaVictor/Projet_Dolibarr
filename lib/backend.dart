46d09f496e6d7357c5799e8398a975588c2f0147
CoumbaBKANE <coumbakane41@gmail.com>
2023-02-22 14:23:44 +0000

    update

--- lib/backend.dart
+++ lib/backend.dart
@@ -1,11 +1,19 @@
 import 'dart:convert';
-
+import '../createCommande.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import '../StockModel.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_session_manager/flutter_session_manager.dart';
 import 'package:intl/intl.dart';
+
+
+Map<String, String> headers = {
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'DOLAPIKEY': '127vu2oyHAs6s8yMf5LeIP7XCEC5dO4k'
+};
+
 
 const api ='https://dolibarr.with6.dolicloud.com/api/index.php/orders';
 
@@ -22,5 +30,24 @@ Future<List<Commandes>> fetchCommandes() async {
     return commandes.map((commandes) => Commandes.fromJson(commandes)).toList();
   } else{
     throw Exception('Impossible de récupérer les commandes');
+  }
+}
+
+Future createCommande(ref, date, note, tiers) async {
+
+  http.Response response = await http.post(Uri.parse(api), headers: headers, body: json.encode({
+    "ref": ref,
+    "date": date,
+
+  }));
+
+  if (response.statusCode == 200) {
+    // La requête a été envoyée avec succès
+    print('La commande a été envoyée avec succès.');
+    return "Ok";
+  } else {
+    // Il y a eu une erreur lors de l'envoi de la requête
+    print('Une erreur est survenue lors de l\'envoi de la commande.');
+    return "No Ok";
   }
 }
